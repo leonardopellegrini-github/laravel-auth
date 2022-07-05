e9c478601c95c3e25f4af960bf5128a79b56510f
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-07-05 18:08:13 +0200

    tutte le rotte vengono protette

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+        ->name('admin.')
+        ->prefix('admin')
+        ->namespace('admin')
+        ->group(function(){
+        Route::get('/', 'HomeController@index')->name('index');
+        });
